f9513c391d40370be46f1de0e1f71e98f0efdf11
iskanderror <iskanderror@gmail.com>
2019-10-03 21:22:13 +0500

    feat: decode binary string

--- src/index.js
+++ src/index.js
@@ -37,8 +37,43 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const TRANSLATE_TABLE = {
+  '**': ' ',
+  '00': '',
+  '10': '.',
+  '11': '-',
+};
+
 function decode(expr) {
     // write your solution here
+    let binaryArray=[];
+    let result='';
+    expressionToArray(expr,binaryArray);
+    for(let binary of binaryArray){
+      let morseCode = binaryToMorse(binary);
+      let letter = MORSE_TABLE[morseCode];
+      if (letter == undefined) {
+        result += ' ';
+      } else {
+        result += letter;
+      }
+    }
+    return result;
+}
+
+function expressionToArray(expr, symbolArray) {
+  for(let i=0; i<expr.length; i+=10) {
+    symbolArray.push(expr.slice(i,i+10));
+  }
+}
+
+function binaryToMorse(binary) {
+  let morseCode = '';
+  for (let i=0; i<binary.length; i+=2){
+    let morseSign = binary.slice(i,i+2);
+    morseCode += TRANSLATE_TABLE[morseSign];
+  }
+  return morseCode;
 }
 
 module.exports = {
